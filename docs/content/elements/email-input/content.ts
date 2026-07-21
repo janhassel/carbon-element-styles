03d68d3d687f1f3a4df613e728765b5398a50b0f
Jan Hassel <jan.hassel@ibm.com>
2026-07-21 11:06:17 +0200

    feat: add email-input element (#132)

---
+++ docs/content/elements/email-input/content.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { CdsEsDocsElementDemoContent } from '@/components/ElementDemoContent';
+
+export { default as css } from './demo.scss?inline';
+export { default as html } from './demo.html';
+export { default as scssDoc } from 'virtual:scss-docs/email-input';
+
+const elementDemoContent = document.createElement('cds-es-docs-element-demo-content') as CdsEsDocsElementDemoContent;
+
+elementDemoContent.label = 'Email input';
+
+elementDemoContent.references = [
+  {
+    label: '<input type="email">',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/email',
+  },
+  {
+    label: 'Text input',
+    url: 'https://carbondesignsystem.com/components/text-input/usage/',
+  },
+];
+
+elementDemoContent.demos.set('default', {});
+elementDemoContent.demos.set('block', { scssConfig: { axis: `'block'` } });
+elementDemoContent.demos.set('inline', { scssConfig: { axis: `'inline'` } });
+
+export default elementDemoContent;
